5b8145f6eb5ead370350e9f72f1ac8f0a60edb73
jeee <45247795+je888@users.noreply.github.com>
2023-12-17 12:19:21 -0800

    datetime error fix

--- process_perks/main.py
+++ process_perks/main.py
@@ -1,5 +1,7 @@
+import logging
 from google.cloud import firestore
-# from flask import escape
+from datetime import datetime
+from dateutil import parser
 
 
 def process_perks(request):
@@ -13,6 +15,38 @@ def process_perks(request):
     else:
         return 'Invalid data format', 400
 
+    # Logging and processing offerExpireDate
+    if 'offerExpireDate' in data:
+        try:
+            # Check if offerExpireDate is already a datetime object
+            if isinstance(data['offerExpireDate'], datetime):
+                # logging.info("offerExpireDate is already a datetime object.")
+                pass
+
+            # If offerExpireDate is a string, try to parse it as a datetime object
+            elif isinstance(data['offerExpireDate'], str):
+                data['offerExpireDate'] = parser.parse(data['offerExpireDate'])
+                # logging.info("Parsed offerExpireDate from string to datetime.")
+
+            # If offerExpireDate is a numeric value, treat it as milliseconds since epoch
+            elif isinstance(data['offerExpireDate'], (int, float)):
+                expire_date_seconds = int(data['offerExpireDate']) // 1000
+                data['offerExpireDate'] = datetime.fromtimestamp(
+                    expire_date_seconds)
+                # logging.info(
+                #     "Converted offerExpireDate from milliseconds to datetime.")
+
+            else:
+                logging.error(
+                    "[process offer] Unrecognized type for offerExpireDate; leaving as is.")
+
+        except Exception as e:
+            logging.error(f"Error parsing offerExpireDate: {e}")
+            logging.info(
+                "Using current datetime as fallback for offerExpireDate.")
+            # Fallback to current datetime
+            data['offerExpireDate'] = datetime.now()
+
     # Initialize Firestore DB
     db = firestore.Client()
     try:
@@ -20,4 +54,5 @@ def process_perks(request):
         doc_ref = db.collection('creditCardPerks').add(data)
         return f'Perk added with ID: {doc_ref[1].id}'
     except Exception as e:
+        logging.error(f'[process offer] Error adding document: {e}')
         return f'Error adding document: {e}', 500
